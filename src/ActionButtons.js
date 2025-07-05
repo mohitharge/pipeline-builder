06eea12e928b5e23ae89153b5f70455af64f0702
MOHIT HARGE <hargemohit@gmail.com>
2025-07-06 02:02:55 +0530

    api url

--- src/ActionButtons.js
+++ src/ActionButtons.js
@@ -63,7 +63,7 @@ export const ActionButtons = ({ nodes, edges, reactFlowInstance }) => {
     setToast(null);
 
     try {
-      const response = await fetch('http://localhost:8000/pipelines/parse', {
+      const response = await fetch('https://your-fastapi-service.onrender.com/pipelines/parse', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ nodes, edges }),
